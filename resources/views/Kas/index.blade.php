eeacc444d6c29df90af1ab2d0566d9f1f5659f61
eko purnomo <ekopurnama115@gmail.com>
2018-11-08 19:11:23 +0700

    eko mari

--- resources/views/Kas/index.blade.php
+++ resources/views/Kas/index.blade.php
@@ -13,4 +13,42 @@
              <div style="margin-left: 3%; font-size: 10px;"> 50000 </div>
         </div>
     </div>
+
+     <div class="chat-list-wrap">
+        <div class="chat-list-adminpro">
+            <div class="chat-button">
+                <span data-toggle="modal" data-target="#input-modal" class="chat-icon-link collapsed" style="background: blue;">
+                    <i class="fa fa-plus" style="margin-top: 30%"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div id="input-modal" class="modal modal-adminpro-general default-popup-PrimaryModal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-close-area modal-close-df">
+                    <a class="close" data-dismiss="modal" >
+                        <i class="fa fa-close" style="margin-top: 25%;"></i>
+                    </a>
+                </div>
+                <div class="modal-body">
+                    <form action="#">
+                        <div class="form-group-inner" align="left">
+                            <label>Nama Kas</label>
+                            <input type="text" id="nama" class="form-control" placeholder="Nama Kas">
+                        </div>
+                        <div class="form-group-inner" align="left">
+                            <label>Saldo</label>
+                            <input type="number" id="harga" class="form-control" placeholder="Saldo">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <a data-dismiss="modal" href="#">Batal</a>
+                    <a href="#" id="btnSave" onclick="addProduk()">Simpan</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/bahan/index.blade.php
+++ resources/views/bahan/index.blade.php
@@ -11,4 +11,47 @@
             <div style="margin-left: 3%; font-size: 18px;"> Lombok </div>
         </div>
     </div>
+    <div class="chat-list-wrap">
+        <div class="chat-list-adminpro">
+            <div class="chat-button">
+                <span data-toggle="modal" data-target="#input-modal" class="chat-icon-link collapsed" style="background: blue;">
+                    <i class="fa fa-plus" style="margin-top: 30%"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div id="input-modal" class="modal modal-adminpro-general default-popup-PrimaryModal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-close-area modal-close-df">
+                    <a class="close" data-dismiss="modal" >
+                        <i class="fa fa-close" style="margin-top: 25%;"></i>
+                    </a>
+                </div>
+
+                <div class="modal-body">
+                    <form action="#">
+                        <div class="form-group-inner" align="left">
+                            <label>Nama Bahan</label>
+                            <input type="text" id="nama" class="form-control" placeholder="Nama Bahan">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <a data-dismiss="modal" href="#">Batal</a>
+                    <a href="#" id="btnSave" onclick="addBahan()">Simpan</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="search-modal" class="modal modal-adminpro-general default-popup-PrimaryModal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-close-area modal-close-df">
+                    <a class="close" data-dismiss="modal" >
+                        <i class="fa fa-close" style="margin-top: 25%;"></i>
+                    </a>
+                </div>
+
 @endsection
